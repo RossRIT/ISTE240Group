b70bad01ab016bbc1930bdb5e716e30a84f8287a
Alec-Jones <aij9281@g.rit.edu>
2023-04-29 10:51:50 -0400

    Content Styling and CSS
    Finalizing styling on the content page and editing styling on the footer. Going to handle the quizzes pages next so that styling is all finalized.

--- start.php
+++ start.php
@@ -24,24 +24,28 @@
                 developed to be adapted for more computers and versions. 
             </p>
         <h3>Unix Architecture</h3>
-            Here’s a basic block diagram of the unix architecture:
+            <p>Here’s a basic block diagram of the unix architecture:</p>
 
             <img src="https://www.urbanpro.com/unix-shell-scripting-training/free-session-on-shell-scripting-for-1-day-9632101509" alt="unix diagram">
-            The goal of this website is to learn the basics of Unix to understand functionality and be able to utilize the system. 
-            In addition to giving an overview of content.
-            (kernel, shell, commands and utilities, files and directories)
+            <p>
+                The goal of this website is to learn the basics of Unix to understand functionality and be able to utilize the system. 
+                In addition to giving an overview of content.
+                (kernel, shell, commands and utilities, files and directories)
+            </p>
 
-        <h2>login + logout</h2>
-            When you first connect to a unix system, it will typically ask for a login like such
-            “login:” By entering in the correct userid and password, you will be allowed to enter into the system. 
-            To logout, simply type in “logout.” 
+        <h3>login + logout</h3>
+            <p>
+                When you first connect to a unix system, it will typically ask for a login like such
+                “login:” By entering in the correct userid and password, you will be allowed to enter into the system. 
+                To logout, simply type in “logout.” 
+            </p>
             </br>
-        
-        looking at files and directories
-            One of the more common uses for Unix is for file management. All data in Unix is organized in files, 
-            which in turn are organized in directories. 
-            Directories are organized into a tree-like structure called the filesystem. 
-            Using the command “ls” you can list all the files or directories available in a directory structure.
+            <p>
+                Looking at files and directories, One of the more common uses for Unix is for file management. All data in Unix is organized in files, 
+                which in turn are organized in directories. 
+                Directories are organized into a tree-like structure called the filesystem. 
+                Using the command “ls” you can list all the files or directories available in a directory structure.
+            </p>
     </div>
 
 </div>
