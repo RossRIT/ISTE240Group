cf0e5a114edc55889aed7bb3cdb73c38e0462872
al2231 <al2231@g.rit.edu>
2023-05-01 16:02:50 -0400

    start

--- start.php
+++ start.php
@@ -7,21 +7,21 @@
 ?>
 <div class="content-container">
     <div class="sidebar">
-    <a href="#" class="sidebar_link">Link 1</a>
-    <a href="#" class="sidebar_link">Link 2</a>
-    <a href="#" class="sidebar_link">Link 3</a>
+    <a href="#unix" class="sidebar_link">UNIX</a>
+    <a href="#arch" class="sidebar_link">Architecture</a>
+    <a href="#login" class="sidebar_link">login/logout</a>
     </div>
 
     <div>
         <h1 class="title">Getting Started</h1>
 
-        <h2>What exactly is UNIX?</h2>
+        <h2 id="unix">What exactly is UNIX?</h2>
             <p>
                 Unix essentially is a multiuser computer operating system. With multiuser capabilities, libraries of software, and a 
                 hierarchical file system; Unix was first developed by AT&T Corportation’s Bell Laboratories in the late 1960s, then 
                 developed to be adapted for more computers and versions. 
             </p>
-        <h3>Unix Architecture</h3>
+        <h3 id="arch">Unix Architecture</h3>
             <p>Here’s a basic block diagram of the unix architecture:</p>
 
             <img src="https://www.tutorialspoint.com/unix/images/unix_architecture.jpg" alt="unix diagram">
@@ -31,7 +31,7 @@
                 (kernel, shell, commands and utilities, files and directories)
             </p>
 
-        <h3>login + logout</h3>
+        <h3 id="login">login + logout</h3>
             <p>
                 When you first connect to a unix system, it will typically ask for a login like such
                 “login:” By entering in the correct userid and password, you will be allowed to enter into the system. 
